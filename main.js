9c3d23b5115c89d6cee6185a83a1f370106d2459
HyunUk Jeong <hjeong326@gmail.com>
2017-06-19 15:44:48 -0500

    Almosted completed random quote machine

---
+++ main.js
@@ -0,0 +1,61 @@
+$("document").ready(function() {
+	var quoteBtn = $("#quoteBtn");
+	var randomQuote = $("#random-quote");
+	var author = $("#author");
+	var tweet = $("#tweet");
+
+	
+
+
+	function updateQuote(data) {
+		randomQuote.html(data.quote);
+		author.text("- " + data.author);
+		var newHref = "https://twitter.com/share?text=\"" + data.quote + "\" " + data.author + "&hashtags=quotes";
+		tweet.attr("href", newHref);
+	}
+
+	function getNewQuote() {
+		$.ajax({
+			headers: {"X-Mashape-Key": "pc5WtyP9MdmshQCnTjhROxZeUgJyp1mHb9Ojsn8Y5LAgQIhjdE"},
+			url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+			success: function(data) {
+				if (typeof updateQuote === 'function') {
+					updateQuote(data);
+				}
+			}
+		});
+	}
+
+	quoteBtn.click(getNewQuote);
+
+
+	getNewQuote();
+
+
+
+// 	quoteBtn.on("click", function() {
+// 		$.ajax({
+// 			headers: {"X-Mashape-Key": "pc5WtyP9MdmshQCnTjhROxZeUgJyp1mHb9Ojsn8Y5LAgQIhjdE"},
+// 			url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+// 			success: function(data) {
+// 				if (typeof updateQuote === 'function') {
+// 					updateQuote(data);
+// 				}
+// 			}
+// 			});
+// 		});
+
+
+
+}); // END OF JQUERY
+
+
+
+// Remember the shorthand
+/*
+
+$(function() {
+
+});
+
+*/

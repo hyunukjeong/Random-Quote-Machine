b638ab718fe3367373e2a3ff75e8969c7c762935
HyunUk Jeong <hjeong326@gmail.com>
2017-06-19 16:43:36 -0500

    added random color

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 $("document").ready(function() {
+
 	var quoteBtn = $("#quoteBtn");
 	var randomQuote = $("#random-quote");
 	var author = $("#author");
 	var tweet = $("#tweet");
 
-	
-
+	var randomColorBackground = $(".random-color-background");
+	var randomColorText = $(".random-color-text");
 
 	function updateQuote(data) {
 		randomQuote.html(data.quote);
 		author.text("- " + data.author);
 		var newHref = "https://twitter.com/share?text=\"" + data.quote + "\" " + data.author + "&hashtags=quotes";
 		tweet.attr("href", newHref);
+		var newColor = getRandomColor();
+		randomColorBackground.css("background-color", newColor);
+		randomColorText.css("color", newColor);
 	}
 
 	function getNewQuote() {
@@ -26,15 +30,23 @@ $("document").ready(function() {
 		});
 	}
 
-	quoteBtn.click(getNewQuote);
+	quoteBtn.on("click", getNewQuote);
 
 
 	getNewQuote();
 
 
-
 }); // END OF JQUERY
 
+
+function getRandomColor() {
+	var letters = "0123456789ABCDEF";
+	var color = "#";
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
 
 
 // Remember the shorthand
